d0139dcaf70984d8c63e7c9db8dc6ab7537a925a
pramodniroshan <pramodnakandara@gmail.com>
2024-06-15 18:57:06 +0530

    uninstall multter

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -33,14 +32,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(cors()); // Enable CORS
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+
 
 app.get('/', (req, res) => {
   res.json({
@@ -48,14 +40,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    return res.status(200).json("File uploaded successfully");
-  } catch (error) {
-    console.error(error);
-  }
-});
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
